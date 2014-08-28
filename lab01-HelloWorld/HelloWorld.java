66f2fa2952afd1248511cb3153534ec6e72ccdfc
tlindberg <tlindberg94@gmail.com>
2014-08-28 18:39:55 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+//Thomas Lindberg
+//Lab 01
+//Hello World Java Program
+
+//define a class
+public class HelloWorld{
+    //add main method
+    public static void main(String[] args) {
+        
+    //print the statement with a box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#   Hello World 2014!! #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    }
+}
+    
+        
