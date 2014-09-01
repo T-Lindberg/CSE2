ff2438134ed77c32add61c09d2514bbad2be4a84
tlindberg <tlindberg94@gmail.com>
2014-09-01 04:19:19 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Thomas Lindberg
+//hw01
+//Welcome Class
+
+//define a class
+public class WelcomeClass{
+
+    //add main method
+    public static void main(String [] args) {
+    
+    //print welcome and message
+    
+    System.out.println("| Welcome |");
+    System.out.println("-----------");
+    System.out.println("^ ^ ^ ^ ^ ^");
+    System.out.println("/\\/\\/\\/\\/\\/\\");
+    System.out.println("|-T-D-L-2-1-6|");
+    System.out.println("\\/\\/\\/\\/\\/\\/");
+    System.out.println("v v v v v v ");
+    System.out.println("My name is Thomas Lindberg and I am a junior ISE major. I am excited to learn programming!");
+    
+    }
+}
