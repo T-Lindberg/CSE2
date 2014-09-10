e53ee4c7956c7941cffd2938030ef70b691b0493
tlindberg <tlindberg94@gmail.com>
2014-09-10 02:06:19 +0000

    updated Arithmetic

---
+++ hw02/Arithmetic.java
@@ -0,0 +1,76 @@
+//Thomas Lindberg
+//hw02
+//Arithmetic
+
+//define a class
+class Arithmetic{
+    
+    //add main method
+    public static void main(String [] args) {
+        
+        //number of paits of socks
+        int nSocks=3;
+        //cost per pair of socks
+        //('$' is part of the variable name)
+        double sockCost$=2.58;
+        
+        //number of drinking glasses
+        int nGlasses=6;
+        //Cost per glass
+        double glassCost$=2.29;
+        
+        //number of boxes of envelopes
+        int nEnvelopes=1;//cost per box of envelopes
+        double envelopeCost$=3.25;
+        double taxPercent=0.06;
+        
+        
+        double totalTax, totalCostTransaction, totalCostPurchases, totalCostSocks, totalCostDG, totalCostEnvelopes, taxSocks, taxDG, taxEnvelopes;
+
+        //Calculating cost of each item in total and tax
+        totalCostSocks= nSocks*sockCost$;
+        taxSocks= taxPercent*totalCostSocks;
+        
+        totalCostDG= nGlasses*glassCost$;
+        taxDG= taxPercent*totalCostDG;
+        
+        
+        totalCostEnvelopes= nEnvelopes*envelopeCost$;
+        taxEnvelopes= totalCostEnvelopes*taxPercent;
+        
+        totalCostPurchases= totalCostSocks + totalCostEnvelopes + totalCostDG;
+        totalTax= taxSocks+taxEnvelopes+taxDG;
+        totalCostTransaction= totalCostPurchases+totalTax;
+        
+        //print item being bought, how many, cost per item, cost total of item, sales tax for items
+        System.out.println("Socks");
+        System.out.println("Number of socks: "+ nSocks);
+        System.out.println("Cost per pair of socks: " + sockCost$);
+        System.out.println("Cost of socks: " +totalCostSocks);
+        System.out.println("Sales tax for socks: " +(int) (taxSocks*100)/100.0); //Refer to page 57 in course textbook for numeric type conversions
+        System.out.println();
+        
+        System.out.println("Drinking Glasses");
+        System.out.println("Number of drinking glasses: " +nGlasses);
+        System.out.println("Cost per drinking glass: "+glassCost$);
+        System.out.println("Cost of drinking glass: " +totalCostDG);
+        System.out.println("Sales tax for drinking glasses: " +(int) (taxDG*100)/100.0); //Refer to page 57 in course textbook for numeric type conversions
+        System.out.println();
+        
+        System.out.println("Envelopes");
+        System.out.println("Number of envelopes: "+nEnvelopes);
+        System.out.println("Cost per envelope: "+envelopeCost$);
+        System.out.println("Cost of envelope: "+totalCostEnvelopes);
+        System.out.println("Sales tax for envelope: "+(int) (taxEnvelopes*100)/100.0); //Refer to page 57 in course textbook for numeric type conversions
+        System.out.println();
+        
+        //print total cost of purchases, total sales tax, total cost of transaction
+        System.out.println("Total cost of the purchases: "+totalCostPurchases);
+        System.out.println("Total sales tax: "+(int) (totalTax*100)/100.0);
+        System.out.println();
+        System.out.println("Total Cost of Transaction: "+(int) (totalCostTransaction*100)/100.0);
+        
+        
+    
+    }
+}
---
+++ lab02-Cyclometer/Cyclometer.java
@@ -0,0 +1,49 @@
+//Thomas Lindberg
+//9/05/2014
+//CSE2
+//Program should: print the number of minutes, number of counts, the distance for each trip, and the distance for two trips
+
+public class Cyclometer{
+    //main method required for every Java program
+    
+    public static void main(String [] args) {
+        
+        int secsTrip1=480; //number of seconds in Trip 1
+        int secsTrip2=3220; //number of seconds in Trip 2
+        int countsTrip1=1561; //number of counts in Trip 1
+        int countsTrip2=9037; //number of counts in Trip 2
+        
+        double wheelDiameter=27.0, //Diameter of the wheel
+        PI=3.14159, // the constant value of PI
+        feetPerMile=5280, // number of feet per mile
+        inchesPerFoot=12, // number of inches per foot
+        secondsPerMinute=60; // number of seconds per minute
+        double distanceTrip1, distanceTrip2, totalDistance; //
+        
+        System.out.println("Trip 1 took "+
+            (secsTrip1/secondsPerMinute)+" minutes and had "+
+            countsTrip1+" counts.");
+        System.out.println("Trip 2 took "+
+        (secsTrip2/secondsPerMinute)+" minutes and had "+
+        countsTrip2+" counts.");
+        
+        //run the calculations; store the values. Document your calculation here
+        //Calculates minutes and number of counts during trips
+        
+        distanceTrip1=countsTrip1*wheelDiameter*PI;
+        //Above gives distance in inches
+        //(for each count, a rotation of the wheel travels
+        //the diameter in inches times PI)
+        distanceTrip1/=inchesPerFoot*feetPerMile;
+        //Gives distance in miles
+        distanceTrip2=countsTrip2*wheelDiamter*PI/inchesPerFoot/feetPerMile;
+        
+        System.out.println("Trip 1 was "+distanceTrip1+" miles");
+        System.out.println("Trip 2 was "+distanceTrip2+" miles");
+        System.out.println("The total distance was "+totalDistance+" miles");
+        //print out the output data
+    }
+    //end of main method
+    
+} //end of class
+
